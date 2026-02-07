e139498b3983683b82c86caa6af31530959755f6
Ruben Acedo <acedoagente@gmail.com>
2026-02-07 14:46:11 +0100

    Add basic operations example using arithmetic functions

---
+++ examples/basic-operations.js
@@ -0,0 +1,6 @@
+import { add, subtract, multiply, divide } from '../src/arithmetic/index.js';
+
+console.log(add(2, 3));       // 5
+console.log(subtract(10, 4)); // 6
+console.log(multiply(3, 7));  // 21
+console.log(divide(20, 5));   // 4
